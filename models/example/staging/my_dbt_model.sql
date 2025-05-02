aa67425cc664d6100a67b16d0fc7f6658b6a0274
atul kumar <atul.kumar9891@gmail.com>
2025-05-02 12:22:14 +0000

    .

--- models/example/staging/my_dbt_model.sql
+++ models/example/staging/my_dbt_model.sql
@@ -5,5 +5,5 @@ WITH tb1  as(
         id ,
         first_name,
         last_name
-     from {{source('datafeed_shared_schema','STG_CUSTOMERS')}})
+     from {{source('datafeed_shared_schema','stg_customers')}})
      select * from tb1

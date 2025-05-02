75a552e2496d0ef19b0458cdc20c452b36bbcc18
atul kumar <atul.kumar9891@gmail.com>
2025-05-02 12:58:31 +0000

    .

--- models/example/staging/order.sql
+++ models/example/staging/order.sql
@@ -3,8 +3,8 @@
 WITH tb1  as(
  select
         id,
-        order_date,
-        user_id
+        order_id,
+        payment_method
         
-     from {{source('datafeed_shared_schema','stg_orders')}})
+     from {{source('datafeed_shared_schema','stg_payments')}})
      select * from tb1

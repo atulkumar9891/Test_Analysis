6444a6afa1b52a5bde2c33b146489fd8beed322c
atul kumar <atul.kumar9891@gmail.com>
2025-06-12 17:26:32 +0000

    .

--- models/example/staging/Order_test_null_uni_accepted.sql
+++ models/example/staging/Order_test_null_uni_accepted.sql
@@ -1,11 +1,10 @@
-{{config(materialized='table',
-pre_hook ="TRUNCATE TABLE{{ this }}",
+{{config(materialized="table",
+pre_hook="TRUNCATE TABLE {{ this }}",
 post_hook="TRUNCATE TABLE {{source('datafeed_shared_schema','stg_customers')}}"
 )
 }}
 
 with tbl_01 as(
     select * from {{source('datafeed_shared_schema','stg_orders')}}
-
-)
+    )
 select * from tbl_01
